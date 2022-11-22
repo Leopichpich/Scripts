431545bbcddeb828cde22d806371a0d050cf75e3
Leopichpich <108740467+Leopichpich@users.noreply.github.com>
2022-11-22 22:05:30 +0100

    Installation SIFT + REMnux on the same client

---
+++ blueteam.sh
@@ -0,0 +1,38 @@
+mkdir blueteam_install
+cd blueteam_install
+siftcli='https://github.com/teamdfir/sift-cli/releases/download/v1.14.0-rc1/sift-cli-linux'
+siftclisig='https://github.com/teamdfir/sift-cli/releases/download/v1.14.0-rc1/sift-cli-linux.sig'
+siftclipub='https://github.com/teamdfir/sift-cli/releases/download/v1.14.0-rc1/sift-cli.pub'
+
+wget $siftcli
+wget $siftclisig
+wget $siftclipub
+
+apt install golang -y
+
+go install github.com/sigstore/cosign/cmd/cosign@latest
+
+cosign verify-blob --key sift-cli.pub --signature sift-cli-linux.sig sift-cli-linux
+
+mv sift-cli-linux /usr/local/bin/sift
+chmod 755 /usr/local/bin/sift
+apt-get update
+
+sift install --mode=packages-only
+
+
+sleep(10)
+
+wget https://REMnux.org/remnux-cli
+
+mv remnux-cli remnux
+chmod +x remnux
+mv remnux /usr/local/bin
+
+apt install -y gnupg
+
+remnux install --mode=addon
+
+
+remnux upgrade
+sift upgrade
